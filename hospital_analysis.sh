ae312d14e42a44cea331b66f53a7bfe77280ce6d
Genereuse3 <g.uwimana@alustudent.com>
2026-09-24 09:32:18 -0700

    add comments explaining pipe-delimited field parsing

--- hospital_analysis.sh
+++ hospital_analysis.sh
@@ -8,6 +8,8 @@ process_vitals() {
 
     for logfile in active_logs/heart_rate_log.log active_logs/temperature_log.log; do
         [ -e "$logfile" ] || continue
+        # Fields are separated by " | " (space-pipe-space) as written by hospital_system.py
+        # $1=Timestamp $2=Device_ID $3=Value (Status is $4, already filtered by grep above)
         grep "CRITICAL" "$logfile" | awk -F' \\| ' '{print $1, "|", $2, "|", $3}' >> reports/critical_alerts.txt
     done
 
